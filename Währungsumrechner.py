76f19aaabf54fb5ee4fee65c40a4f4f7efe30bd2
Duncan Lohmann <duncanzick8@gmail.com>
2025-01-28 08:51:18 +0100

    Add files via upload

---
+++ Währungsumrechner.py
@@ -0,0 +1,8 @@
+print("Herzlich Willkommen zum Währungsrechner")
+print("")
+
+
+euro = input("Bitte betrag in Euro Eingeben: ")
+dollar = 1.07 * float(euro)
+
+print(f"Der Betrag entspricht {dollar}")
